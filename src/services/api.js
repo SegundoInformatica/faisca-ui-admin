49f3274e08fa7789cdd2ad16fab78507bd04d45c
Nathan Silva <nathan.silva26@gmail.com>
2024-02-24 20:48:41 -0300

    fix route api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({    
-    //baseURL: 'http://44.207.2.85:8080/'    
-    baseURL: 'http://localhost:8080/'    
+    baseURL: 'http://44.207.2.85:8080/'    
+    //baseURL: 'http://localhost:8080/'    
 });
 api.interceptors.request.use(
     async (config) => {
